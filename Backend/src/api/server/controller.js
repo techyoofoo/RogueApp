283a47b7f21c94e43e8c34402a7f69bc67b00f6d
abhaymcauuv <abhaymcauuv@gmail.com>
2019-10-14 19:38:04 +0530

    Server Connection String

---
+++ Backend/src/api/server/controller.js
@@ -0,0 +1,55 @@
+import serverSchema from "./model";
+import mongoose, { Schema } from "mongoose";
+
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+export const create = async (req, res) => {
+    let conString = `server=${req.body.server};database=${req.body.database};uid=${req.body.userid};pwd=${req.body.password};pooling=${req.body.pooling};`
+    var encrypted = encrypt(conString);
+    const createServer = new serverSchema({ name: req.body.name, iv: encrypted.iv, key: encrypted.key, connection: encrypted.encryptedData });
+    await createServer
+        .save()
+        .then(data => {
+            res.send({ status: 200, id: data._id, message: "Saved successfully" });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "error occurred while creating."
+            });
+        });
+};
+
+export const findServerById = async (req, res) => {
+    await serverSchema.findById({ _id: req.params.id })
+        .then(ServerInfo => {
+            let conString = decrypt(ServerInfo);
+            res.send(conString);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            });
+        });
+}
+
+
+
+function encrypt(text) {
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), key: key.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.connection, 'hex');
+    let key = Buffer.from(text.key, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
---
+++ Backend/src/api/server/model.js
@@ -0,0 +1,46 @@
+import baseschema from '../baseschema'
+const extendSchema = require('mongoose-extend-schema');
+import mongoose, { Schema } from "mongoose";
+
+
+const serverSchema = extendSchema(baseschema,
+    {
+        name: {
+            type: String
+        },
+        iv: {
+            type: String
+        },
+        key: {
+            type: String
+        },
+        connection: {
+            type: String
+        }
+    }
+)
+
+serverSchema.methods = {
+    view(full) {
+        const view = {
+            id: this.id,
+            name: this.name,
+            iv: this.iv,
+            key: this.key,
+            connection: this.connection
+        };
+
+        return full
+            ? {
+                ...view
+                // add properties for a full view
+            }
+            : view;
+    }
+};
+
+const model = mongoose.model("server", serverSchema);
+
+export const schema = model.schema;
+export default model;
+
